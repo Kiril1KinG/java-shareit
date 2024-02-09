7b85abf609f901121decb514731807ef9a3b2e65
Кирилл Максимович <kirillking01@yandex.ru>
2024-02-09 21:08:57 +0300

    refactor: заменить обращения по индексу на имя параметра

--- src/main/java/ru/practicum/shareit/item/storage/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/storage/ItemRepository.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.item.storage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.practicum.shareit.item.entity.ItemEntity;
 
 import java.util.Collection;
@@ -12,8 +13,8 @@ public interface ItemRepository extends JpaRepository<ItemEntity, Integer> {
     Collection<ItemEntity> findUsersByOwnerIdOrderByIdAsc(Integer ownerId);
 
     @Query("select i from ItemEntity i " +
-            "where (upper(i.name) like upper(concat('%', ?1, '%')) " +
-            " or upper(i.description) like upper(concat('%', ?1, '%'))) " +
+            "where (upper(i.name) like upper(concat('%', :text, '%')) " +
+            " or upper(i.description) like upper(concat('%', :text, '%'))) " +
             "and i.available = true")
-    List<ItemEntity> search(String text);
+    List<ItemEntity> search(@Param("text") String text);
 }
